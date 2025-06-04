262d7d5ccfc30f6a2b0a79a50f626e416bbc8bda
idrisnergis <drsnrg@gmail.com>
2025-06-04 11:00:42 +0300

    inventory-service Client Method add

--- order-service/src/main/java/com/idrisnergis/microservice/order_service/service/OrderService.java
+++ order-service/src/main/java/com/idrisnergis/microservice/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.idrisnergis.microservice.order_service.service;
 
+import com.idrisnergis.microservice.order_service.client.InventoryClient;
 import com.idrisnergis.microservice.order_service.dto.OrderRequest;
 import com.idrisnergis.microservice.order_service.model.Order;
 import com.idrisnergis.microservice.order_service.repository.OrderRepository;
@@ -14,13 +15,21 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
-        Order order   = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(order.getPrice());
-        order.setSkuCode(order.getSkuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
+
+        if(isProductInStock){
+            Order order   = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(order.getPrice());
+            order.setSkuCode(order.getSkuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        }else {
+            throw  new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
